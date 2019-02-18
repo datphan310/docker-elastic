67203bebbe2f23d91df1e0b658c5740051500d66
datphan-sts <datphan@saigontechnology.com>
2019-02-19 02:55:07 +0700

    .

--- deployStack-Beats.sh
+++ deployStack-Beats.sh
@@ -4,6 +4,7 @@ export ELASTIC_VERSION=6.6.0
 export ELASTICSEARCH_USERNAME=elastic
 export ELASTICSEARCH_PASSWORD=changeme
 export ELASTICSEARCH_HOST=swarm-manager
+export KIBANA_HOST=swarm-manager
 
 docker network create --driver overlay --attachable elastic
 docker stack deploy --compose-file docker-compose.yml elastic
